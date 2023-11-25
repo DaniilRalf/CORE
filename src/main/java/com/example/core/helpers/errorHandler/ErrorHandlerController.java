f3850180db7216d8816700927eb63188076eeffa
DaniilRalf <ralf.danya@mail.ru>
2023-11-25 14:37:51 +0700

    fix Interceptor

--- src/main/java/com/example/core/helpers/errorHandler/ErrorHandlerController.java
+++ src/main/java/com/example/core/helpers/errorHandler/ErrorHandlerController.java
@@ -2,7 +2,6 @@ package com.example.core.helpers.errorHandler;
 
 import com.example.core.dto.ResponseErrorData;
 import com.example.core.helpers.errorHandler.exceptions.MainException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,7 +12,7 @@ public class ErrorHandlerController {
     @ExceptionHandler(MainException.class)
     public ResponseEntity<ResponseErrorData> notFoundException(MainException exception) {
         return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
+                .status(exception.getCode())
                 .body(new ResponseErrorData(exception.getMessage(), exception.getCode()));
     }
 
